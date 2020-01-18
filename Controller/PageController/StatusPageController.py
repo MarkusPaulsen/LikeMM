13dded47a01e8d470d55812a4a1494ab3b80514b
HiItsLuis <luisferponfe@gmail.com>
2020-01-18 23:18:58 +0100

    Artist list check

--- Controller/PageController/StatusPageController.py
+++ Controller/PageController/StatusPageController.py
@@ -108,7 +108,7 @@ class StatusPageController:
             else:
                 self.load_lists()
 
-            print("Artist type: " + str(type(self.artist_list)))
+            print("Artist type: " + str(type(self.artist_list[0])))
             self.artist_list.sort(key=lambda artist: artist["name"])
             self.movie_list.sort(key=lambda movie: movie["title"])
 

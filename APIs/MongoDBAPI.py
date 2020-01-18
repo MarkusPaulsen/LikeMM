6d8ecdab75def16d97d9a34011b14a3aa71bc49d
HiItsLuis <luisferponfe@gmail.com>
2020-01-18 22:39:57 +0100

    fixed self.mongo_pass

--- APIs/MongoDBAPI.py
+++ APIs/MongoDBAPI.py
@@ -11,7 +11,7 @@ class MongoDBAPI:
     # <editor-fold desc="Constructor">
     def __init__(self):
         self.mongo_pass: str = os.environ["mongo_pass"]
-        self.mongo_db_client = pymongo.MongoClient("mongodb+srv://HiItsLuis:" + mongo_pass + "@fermovies-kdmcj.mongodb.net/test?retryWrites=true&w=majority")
+        self.mongo_db_client = pymongo.MongoClient("mongodb+srv://HiItsLuis:" + self.mongo_pass + "@fermovies-kdmcj.mongodb.net/test?retryWrites=true&w=majority")
         #self.mongo_db_client = pymongo.MongoClient("mongodb://localhost:27017/")
         if "SNLLAB1DB" in self.mongo_db_client.list_database_names():
             self.sn_lab1_db = self.mongo_db_client["SNLLAB1DB"]

f05f105afc755fc7d43337730e565d2c30d81798
Markus Paulsen <ga96yen@mytum.de>
2020-01-18 21:10:40 +0100

    Finished Movie Endpoint first version

--- Model/Movie.py
+++ Model/Movie.py
@@ -1,5 +1,7 @@
 # <editor-fold desc="Libaries">
 from typing import *
+
+
 # </editor-fold>
 
 
@@ -7,10 +9,43 @@ class Movie:
 
     # <editor-fold desc="Description">
     def __init__(self, themoviedb_input: dict):
-        self.original_title: str = themoviedb_input["original_title"]
+        self.title: str = themoviedb_input["title"]
+
+        self.themoviedb_status: str = str(themoviedb_input["status"])
+        self.themoviedb_runtime: int = str(themoviedb_input["runtime"])
+        self.themoviedb_budget: int = str(themoviedb_input["budget"])
+        self.themoviedb_revenue: int = str(themoviedb_input["revenue"])
+        self.themoviedb_release_date: str = str(themoviedb_input["release_date"])
+        self.themoviedb_backdrop_path: str = str("https://image.tmdb.org/t/p/w500") + str(
+            themoviedb_input["backdrop_path"])
+
+        self.themoviedb_url = str("https://www.themoviedb.org/movie/") + str(themoviedb_input["id"])
+
+        self.themoviedb_popularity: str = str(themoviedb_input["popularity"])
+        self.themoviedb_vote_average: str = str(themoviedb_input["vote_average"])
+        self.themoviedb_genres: List[str] = list(
+            map(
+                lambda genre: str(genre["name"]),
+                themoviedb_input["genres"]
+            )
+        )
 
     def json(self):
         return {
-            "original_title": self.original_title,
+            "title": self.title,
+
+            "themoviedb_status": self.themoviedb_status,
+            "themoviedb_runtime": self.themoviedb_runtime,
+            "themoviedb_budget": self.themoviedb_budget,
+            "themoviedb_revenue": self.themoviedb_revenue,
+            "themoviedb_release_date": self.themoviedb_release_date,
+
+            "themoviedb_url": self.themoviedb_url,
+            "themoviedb_backdrop_path": self.themoviedb_backdrop_path,
+
+            "themoviedb_popularity": self.themoviedb_popularity,
+            "themoviedb_vote_average": self.themoviedb_vote_average,
+            "themoviedb_genres": self.themoviedb_genres
+
         }
     # </editor-fold>

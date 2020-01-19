5ed0a4d49671d4b62a28110b04a99bada6ec7613
Markus Paulsen <ga96yen@mytum.de>
2020-01-19 22:51:35 +0100

    Fixed reload

--- static/updateStatus.js
+++ static/updateStatus.js
@@ -1,14 +1,6 @@
 $(document).ready(function() {
     $('.updateButton').on('click', function() {
         $('.loader-wrapper').addClass('is-active');
-        $.ajax({
-            type : 'GET',
-            url : '/RebuildStatus/'.concat(fid),
-            success : console.log(fid)
-            })
-        .done(function(data) {
-            $('.loader-wrapper').removeClass('is-active');
-            window.location.reload(true); 
-        });
+        window.location.replace("/RebuildStatus/" + fid);
     });
 });

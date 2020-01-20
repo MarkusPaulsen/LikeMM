9af81d9911bae1763582afeb80e26bd130010c4a
Markus Paulsen <ga96yen@mytum.de>
2020-01-20 17:20:29 +0100

    Final Fixes

--- Controller/EndpointController/UserGenresEPController.py
+++ Controller/EndpointController/UserGenresEPController.py
@@ -8,12 +8,21 @@ class UserGenresEPController:
         self.mongodb_api: MongoDBAPI = MongoDBAPI()
         self.genre_list = []
 
+    def get_first_element(self, element_list):
+        try:
+            return [element_list[0]]
+        except Exception:
+            return []
+
     def load_genre_list(self):
         query_element = list(self.mongodb_api.query_movie_db(
             selection={"id": self.fid},
             projection={"themoviedb_genres": 1}
         ))
-        genre_list_of_lists = list(map(lambda genre_list: [genre_list["themoviedb_genres"][0]], query_element))
+        genre_list_of_lists = list(map(
+            lambda genre_list: self.get_first_element(genre_list["themoviedb_genres"]),
+            query_element
+        ))
         genre_list_not_unique = [genre for genre_list in genre_list_of_lists for genre in genre_list]
         self.genre_list = list(set(genre_list_not_unique))
 
